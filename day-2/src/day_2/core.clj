2e7ca6fdb5de7437f911633884dc839ead5954e0
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-02 23:34:56 +0100

    Resuelto el 1er problema del dia 2

--- day-2/src/day_2/core.clj
+++ day-2/src/day_2/core.clj
@@ -1,7 +1,68 @@
 (ns day-2.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as st]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; extraer-componente :: [String] -> String
+(defn extraer-componente [lista color]
+  (let [lista-color (filter #(st/ends-with? % color) lista)
+        lista-color-arreglada (if (= '() lista-color)
+                                '("0")
+                                lista-color)]
+    (-> lista-color-arreglada
+        (first)
+        (st/split #" ")
+        (first))))
+
+; obtener-componente :: String -> [Integer]
+(defn obtener-componente [cadena]
+  (let [lista (->> (st/split cadena #",")
+                   (map st/trim))
+        cad-rojos (extraer-componente lista "red")
+        cad-verdes (extraer-componente lista "green")
+        cad-azules (extraer-componente lista "blue")
+        num-rojos (Integer/parseInt cad-rojos)
+        num-verdes (Integer/parseInt cad-verdes)
+        num-azules (Integer/parseInt cad-azules)]
+    [num-rojos num-verdes num-azules]))
+
+; obtener-componentes :: [String] -> [[Integer]]
+(defn obtener-componentes [lista]
+  (map obtener-componente lista))
+
+; obtener-maximos :: String -> [Integer]
+(defn obtener-maximos [cadena]
+  (let [division-cadena (st/split cadena #";")
+        componentes (obtener-componentes division-cadena)
+        max-rojos (reduce (fn [acc elt] (max acc (nth elt 0))) 0 componentes) 
+        max-verdes (reduce (fn [acc elt] (max acc (nth elt 1))) 0 componentes)
+        max-azules (reduce (fn [acc elt] (max acc (nth elt 2))) 0 componentes)]
+    [max-rojos max-verdes max-azules]))
+
+; procesar-fila :: String -> Int
+(defn procesar-fila [fila]
+  (let [division-cadena (st/split fila #":")
+        id-juego (-> division-cadena
+                         (first)
+                         (st/trim)
+                         (st/split #" ")
+                         (second))
+        num-juego (Integer/parseInt id-juego)
+        resto-cadena (second division-cadena)
+        maximos (obtener-maximos resto-cadena)
+        max-rojos (first maximos)
+        max-verdes (second maximos)
+        max-azules (nth maximos 2)]
+    (if (and (<= max-rojos 12)
+             (<= max-verdes 13)
+             (<= max-azules 14))
+      num-juego
+      0)))
+
+(defn -main [& args]
+  (let [salida-tarea (->> "./resources/input.lst"
+                          (slurp)
+                          (st/split-lines)
+                          (map procesar-fila))
+        tarea-1 (->> salida-tarea
+                     (reduce + 0))]
+    (println tarea-1)))
--- day-2/test/day_2/core_test.clj
+++ day-2/test/day_2/core_test.clj
@@ -1,7 +1,27 @@
 (ns day-2.core-test
-  (:require [clojure.test :refer :all]
-            [day-2.core :refer :all]))
+  (:require [clojure.test :as test]
+            [day-2.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(test/deftest a-test
+  (test/testing "probando procesar-fila"
+    (test/is (= (core/procesar-fila "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") 1))
+    (test/is (= (core/procesar-fila "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue") 2))
+    (test/is (= (core/procesar-fila "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red") 0))
+    (test/is (= (core/procesar-fila "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red") 0))
+    (test/is (= (core/procesar-fila "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green") 5)))
+  (test/testing "probando obtener-maximos"
+    (test/is (= (core/obtener-maximos "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") [4 2 6]))
+    (test/is (= (core/obtener-maximos "1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue") [1 3 4]))
+    (test/is (= (core/obtener-maximos "8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red") [20 13 6]))
+    (test/is (= (core/obtener-maximos "1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red") [14 3 15]))
+    (test/is (= (core/obtener-maximos "6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green") [6 3 2])))
+  (test/testing "probando obtener-componentes"
+    (test/is (= (core/obtener-componentes ["3 blue, 4 red" "1 red, 2 green, 6 blue" "2 green"]) '([4 0 3] [1 2 6] [0 2 0])))
+    (test/is (= (core/obtener-componentes ["1 blue, 2 green" "3 green, 4 blue, 1 red" "1 green, 1 blue"]) '([0 2 1] [1 3 4] [0 1 1])))
+    (test/is (= (core/obtener-componentes ["8 green, 6 blue, 20 red" "5 blue, 4 red, 13 green" "5 green, 1 red"]) '([20 8 6] [4 13 5] [1 5 0])))
+    (test/is (= (core/obtener-componentes ["1 green, 3 red, 6 blue" "3 green, 6 red" "3 green, 15 blue, 14 red"]) '([3 1 6] [6 3 0] [14 3 15])))
+    (test/is (= (core/obtener-componentes ["6 red, 1 blue, 3 green"  "2 blue, 1 red, 2 green"]) '([6 3 1] [1 2 2]))))
+  (test/testing "probando obtener-componente"
+    (test/is (= (core/obtener-componente "3 blue, 4 red") [4 0 3]))
+    (test/is (= (core/obtener-componente "1 red, 2 green, 6 blue") [1 2 6]))
+    (test/is (= (core/obtener-componente "2 green") [0 2 0]))))
