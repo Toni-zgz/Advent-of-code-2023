61667bd067c190f242b6bbc517052e32be57eccf
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-03 17:07:00 +0100

    Resuelta tarea 2 del dia 1

--- day-1/src/day_1/core.clj
+++ day-1/src/day_1/core.clj
@@ -2,18 +2,36 @@
   (:gen-class)
   (:require [clojure.string :as st]))
 
-; procesar-fila :: String -> Int
-(defn procesar-fila [fila]
+; procesar-tarea1 :: String -> Int
+(defn procesar-tarea-1 [fila]
   (let [lista-numeros (filter #(Character/isDigit %) fila)
         primer-numero (first lista-numeros)
         ultimo-numero (last lista-numeros)
         cadena-numeros (str primer-numero ultimo-numero)]
     (Integer/parseInt cadena-numeros)))
 
+; procesar-tarea2 :: String -> Int
+(defn procesar-tarea-2 [fila]
+  (-> fila
+      (st/replace #"one" "o1e")   ; sustituyo la palabra por el numero manteniendo 
+      (st/replace #"two" "t2o")   ; las letras inicial y final de la palabra
+      (st/replace #"three" "t3e") ; en vez de sustituir la palabra en ingles por el 
+      (st/replace #"four" "f4r")  ; numero para evitar que se pierdan numeros
+      (st/replace #"five" "f5e")  ; anteriores que esten en una regla inferior.
+      (st/replace #"six" "s6x")
+      (st/replace #"seven" "s7n")
+      (st/replace #"eight" "e8t")
+      (st/replace #"nine" "n9e")
+      (st/replace #"zero" "z0o")
+      (procesar-tarea-1)))
+
 (defn -main [& args]
   (let [entrada-tareas (->> "./resources/input.lst"
                             (slurp)
                             (st/split-lines))
-        salida-tarea-1 (map procesar-fila entrada-tareas)
-        tarea-1 (reduce + 0 salida-tarea-1)]
-    (println tarea-1)))
+        salida-tarea-1 (map procesar-tarea-1 entrada-tareas)
+        salida-tarea-2 (map procesar-tarea-2 entrada-tareas)
+        tarea-1 (reduce + 0 salida-tarea-1)
+        tarea-2 (reduce + 0 salida-tarea-2)]
+    (println tarea-1)
+    (println tarea-2)))
--- day-1/test/day_1/core_test.clj
+++ day-1/test/day_1/core_test.clj
@@ -3,8 +3,23 @@
             [day-1.core :refer :all]))
 
 (deftest day-1-test
-  (testing "probando procesar-fila"
-    (is (= (procesar-fila "1abc2") 12))
-    (is (= (procesar-fila "pqr3stu8vwx") 38))
-    (is (= (procesar-fila "a1b2c3d4e5f") 15))
-    (is (= (procesar-fila "treb7uchet") 77))))
+  (testing "probando procesar-tarea-1"
+    (is (= (procesar-tarea-1 "1abc2") 12))
+    (is (= (procesar-tarea-1 "pqr3stu8vwx") 38))
+    (is (= (procesar-tarea-1 "a1b2c3d4e5f") 15))
+    (is (= (procesar-tarea-1 "treb7uchet") 77)))
+  (testing "probando procesar-tarea-2"
+    (is (= (procesar-tarea-2 "two1nine") 29))
+    (is (= (procesar-tarea-2 "eightwothree") 83))
+    (is (= (procesar-tarea-2 "abcone2threexyz") 13))
+    (is (= (procesar-tarea-2 "xtwone3four") 24))
+    (is (= (procesar-tarea-2 "4nineeightseven2") 42))
+    (is (= (procesar-tarea-2 "zoneight234") 14))
+    (is (= (procesar-tarea-2 "7pqrstsixteen") 76))))
+
+
+
+
+
+
+
