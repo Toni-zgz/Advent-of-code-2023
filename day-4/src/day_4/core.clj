5d333c36e7324652ad94bd2f47dd246932c1d277
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-04 23:40:45 +0100

    Resuelta tarea 1 dia 4

--- day-4/src/day_4/core.clj
+++ day-4/src/day_4/core.clj
@@ -1,7 +1,51 @@
 (ns day-4.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as st])
+  (:require [clojure.math :as math]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; obtener-tarjetas :: String -> [String]
+(defn obtener-tarjetas [cadena]
+  (->>
+   (-> cadena
+       (st/split #":")
+       (second)
+       (st/trim)
+       (st/split #"\|"))
+   (map st/trim)))
+
+; incluido-en-lista? :: Int -> [Int] -> Bool
+(defn incluido-en-lista? [valor lista]
+  (let [mapa (zipmap lista lista)]
+    (not (nil? (mapa valor)))))
+
+; comprobar-tarjetas :: String -> String -> Int
+(defn comprobar-tarjetas [cad-ganadora cad-mia]
+  (let [lista-ganadora (->> (st/split cad-ganadora #" ")
+                            (remove #(= % ""))
+                            (map #(Integer/parseInt %))) 
+        lista-mia (->> (st/split cad-mia #" ")
+                       (remove  #(= % ""))
+                       (map #(Integer/parseInt %)))]
+    (-> (filter #(incluido-en-lista? % lista-ganadora) lista-mia)
+        (count))))
+
+; obtener-puntuacion :: Int -> Int
+(defn obtener-puntuacion [numero]
+  (int (math/pow 2 (- numero 1))))
+
+; procesar-tarea-1 :: String -> Int
+(defn procesar-tarea-1 [fila]
+  (let [tarjetas (obtener-tarjetas fila)
+        ganadora (first tarjetas)
+        mia (second tarjetas)
+        valor (comprobar-tarjetas ganadora mia)]
+    (obtener-puntuacion valor)))
+
+(defn -main [& args]
+  (let [entrada-tareas (->> "./resources/input.lst"
+                            (slurp)
+                            (st/split-lines))
+        salida-tarea-1 (map procesar-tarea-1 entrada-tareas) 
+        tarea-1 (->> salida-tarea-1
+                     (reduce + 0))] 
+    (println tarea-1)))
--- day-4/test/day_4/core_test.clj
+++ day-4/test/day_4/core_test.clj
@@ -1,7 +1,33 @@
 (ns day-4.core-test
-  (:require [clojure.test :refer :all]
-            [day-4.core :refer :all]))
+  (:require [clojure.test :as test]
+            [day-4.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(test/deftest day-4-test
+  (test/testing "Probando obtener-tarjetas"
+    (test/is (= (core/obtener-tarjetas "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53") ["41 48 83 86 17" "83 86  6 31 17  9 48 53"]))
+    (test/is (= (core/obtener-tarjetas "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19") ["13 32 20 16 61" "61 30 68 82 17 32 24 19"]))
+    (test/is (= (core/obtener-tarjetas "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1") ["1 21 53 59 44" "69 82 63 72 16 21 14  1"]))
+    (test/is (= (core/obtener-tarjetas "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83") ["41 92 73 84 69" "59 84 76 51 58  5 54 83"]))
+    (test/is (= (core/obtener-tarjetas "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36") ["87 83 26 28 32" "88 30 70 12 93 22 82 36"]))
+    (test/is (= (core/obtener-tarjetas "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11") ["31 18 13 56 72" "74 77 10 23 35 67 36 11"])))
+  (test/testing "Probando incluido-en-lista?"
+    (test/is (= (core/incluido-en-lista? 41 [83 86 6 31 17 9 48 53]) false))
+    (test/is (= (core/incluido-en-lista? 83 [83 86 6 31 17 9 48 53]) true)))
+  (test/testing "Probando comprobar-tarjetas"
+    (test/is (= (core/comprobar-tarjetas "41 48 83 86 17" "83 86  6 31 17  9 48 53") 4))
+    (test/is (= (core/comprobar-tarjetas "13 32 20 16 61" "61 30 68 82 17 32 24 19") 2))
+    (test/is (= (core/comprobar-tarjetas "1 21 53 59 44" "69 82 63 72 16 21 14  1") 2))
+    (test/is (= (core/comprobar-tarjetas "41 92 73 84 69" "59 84 76 51 58  5 54 83") 1))
+    (test/is (= (core/comprobar-tarjetas "87 83 26 28 32" "88 30 70 12 93 22 82 36") 0))
+    (test/is (= (core/comprobar-tarjetas "31 18 13 56 72" "74 77 10 23 35 67 36 11") 0)))
+  (test/testing "Probando procesar-tarea-1"
+    (test/is (= (core/procesar-tarea-1 "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53") 8))
+    (test/is (= (core/procesar-tarea-1 "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19") 2))
+    (test/is (= (core/procesar-tarea-1 "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1") 2))
+    (test/is (= (core/procesar-tarea-1 "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83") 1))
+    (test/is (= (core/procesar-tarea-1 "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36") 0))
+    (test/is (= (core/procesar-tarea-1 "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11") 0)))
+  (test/testing "Probando obtener-puntuacion"
+    (test/is (= (core/obtener-puntuacion 1) 1))
+    (test/is (= (core/obtener-puntuacion 2) 2))
+    (test/is (= (core/obtener-puntuacion 3) 4))))
