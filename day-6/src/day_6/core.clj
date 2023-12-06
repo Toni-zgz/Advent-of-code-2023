519ab245fda6f334c61a75f64792cc50152cad1d
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-06 16:34:54 +0100

    Resuelta tarea 1 dia 6

--- day-6/src/day_6/core.clj
+++ day-6/src/day_6/core.clj
@@ -1,7 +1,42 @@
 (ns day-6.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as st]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; obtener-distancia :: Int -> Int -> Int
+(defn obtener-distancia [tiempo-total tiempo-carga]
+  (let [velocidad tiempo-carga
+        tiempo-carrera (- tiempo-total tiempo-carga)]
+    (* velocidad tiempo-carrera)))
+
+; obtener-numero-jugadas-optimas :: Int -> Int -> Int
+(defn obtener-numero-jugadas-optimas [tiempo distancia]
+  (let [lista-tiempos-carga (range 1 tiempo)
+        lista-distancias (map #(obtener-distancia tiempo %) lista-tiempos-carga)
+        lista-distancias-optimas (filter #(> % distancia) lista-distancias)]
+    (count lista-distancias-optimas)))
+
+; obtener-lista :: String -> [Int]
+(defn obtener-lista [cadena]
+  (->>
+   (-> cadena
+       (st/split #":")
+       (second)
+       (st/trim)
+       (st/split #" "))
+   (filter #(not (= "" %)))
+   (map #(Integer/parseInt %))))
+
+; procesar-tarea-1 :: [String] -> [Int]
+(defn procesar-tarea-1 [filas]
+  (let [tiempo (obtener-lista (first filas))
+        distancia (obtener-lista (second filas))]
+    (map obtener-numero-jugadas-optimas tiempo distancia)))
+
+(defn -main [& args]
+  (let [entrada-tareas (->> "./resources/input.lst"
+                             (slurp)
+                             (st/split-lines))
+         salida-tarea-1 (procesar-tarea-1 entrada-tareas)
+         tarea-1 (reduce * 1 salida-tarea-1)]
+    (println salida-tarea-1) 
+    (println tarea-1)))
--- day-6/test/day_6/core_test.clj
+++ day-6/test/day_6/core_test.clj
@@ -1,7 +1,21 @@
 (ns day-6.core-test
-  (:require [clojure.test :refer :all]
-            [day-6.core :refer :all]))
+  (:require [clojure.test :as test]
+            [day-6.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(test/deftest day-6-test
+  (test/testing "probando obtener-distancia"
+    (test/is (= (core/obtener-distancia 7 0) 0))
+    (test/is (= (core/obtener-distancia 7 1) 6))
+    (test/is (= (core/obtener-distancia 7 2) 10))
+    (test/is (= (core/obtener-distancia 7 3) 12))
+    (test/is (= (core/obtener-distancia 7 4) 12))
+    (test/is (= (core/obtener-distancia 7 5) 10))
+    (test/is (= (core/obtener-distancia 7 6) 6))
+    (test/is (= (core/obtener-distancia 7 7) 0)))
+  (test/testing "probando obtener-numero-jugadas-optimas"
+    (test/is (= (core/obtener-numero-jugadas-optimas 7 9) 4))
+    (test/is (= (core/obtener-numero-jugadas-optimas 15 40) 8))
+    (test/is (= (core/obtener-numero-jugadas-optimas 30 200) 9)))
+  (test/testing "probandoo obtener-lista"
+    (test/is (= (core/obtener-lista "Time:      7  15   30") [7 15 30]))
+    (test/is (= (core/obtener-lista "Distance:  9  40  200") [9 40 200]))))
