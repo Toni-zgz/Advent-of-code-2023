1d301a90984a2c2ba535a53f21cf5d9572cab22b
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-08 20:30:11 +0100

    Solucionado tarea 1 del dia 8

--- day-8/src/day_8/core.clj
+++ day-8/src/day_8/core.clj
@@ -1,7 +1,52 @@
 (ns day-8.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as st]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; obtener-numero-ciclos :: String -> {String, [String String]} -> Int
+(defn obtener-numero-ciclos [cadena nodos]
+  (loop [numero-ciclos 0
+         indice-programa 0
+         nodo "AAA"]
+    (if (= nodo "ZZZ")
+      numero-ciclos
+      (let [nuevo-indice-programa (if (= indice-programa (- (count cadena) 1))
+                                    0
+                                    (+ indice-programa 1))
+            direccion (-> (get cadena indice-programa)
+                          (str))
+            nuevo-nodo (if (= direccion "L")
+                         (-> (get nodos nodo)
+                             (first))
+                         (-> (get nodos nodo)
+                             (second)))]
+        (recur (+ numero-ciclos 1) nuevo-indice-programa nuevo-nodo)))))
+
+; obtener-datos :: [String] -> {String, [String String]}
+(defn obtener-datos [lista-cadenas]
+  (loop [datos {}
+         lista lista-cadenas]
+    (if (= lista '())
+      datos
+      (let [cadena (first lista)
+            lista-clave-valor (st/split cadena #" =")
+            clave (-> (first lista-clave-valor)
+                      (st/trim))
+            valor (-> (second lista-clave-valor) 
+                      (st/replace "(" "")
+                      (st/replace ")" " ")
+                      (st/trim)
+                      (st/split #", "))
+            nuevos-datos (assoc datos clave valor)]
+        (recur nuevos-datos (rest lista))))))
+
+(defn procesar-tarea-1 [lista-filas]
+  (let [cadena-programa (first lista-filas)
+        dict-nodos (obtener-datos (drop 2 lista-filas))]
+    (obtener-numero-ciclos cadena-programa dict-nodos)))
+
+(defn -main [& args]
+  (let [entrada-tareas (->> "./resources/input.lst"
+                            (slurp)
+                            (st/split-lines))
+        salida-tarea-1 (procesar-tarea-1 entrada-tareas)]
+    (println salida-tarea-1)))
--- day-8/test/day_8/core_test.clj
+++ day-8/test/day_8/core_test.clj
@@ -1,7 +1,35 @@
 (ns day-8.core-test
-  (:require [clojure.test :refer :all]
-            [day-8.core :refer :all]))
+  (:require [clojure.test :as test]
+            [day-8.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(test/deftest day-8-test
+  (test/testing "probando obtener-datos"
+    (test/is (= (core/obtener-datos ["AAA = (BBB, CCC)"
+                                "BBB = (DDD, EEE)"
+                                "CCC = (ZZZ, GGG)"
+                                "DDD = (DDD, DDD)"
+                                "EEE = (EEE, EEE)"
+                                "GGG = (GGG, GGG)"
+                                "ZZZ = (ZZZ, ZZZ)"]) 
+                {"AAA" ["BBB" "CCC"]
+                 "BBB" ["DDD" "EEE"]
+                 "CCC" ["ZZZ" "GGG"]
+                 "DDD" ["DDD" "DDD"]
+                 "EEE" ["EEE" "EEE"]
+                 "GGG" ["GGG" "GGG"]
+                 "ZZZ" ["ZZZ" "ZZZ"]})))
+  (test/testing "probando procesar-tarea-1"
+    (test/is (= (core/procesar-tarea-1 ["RL"
+                                        ""
+                                        "AAA = (BBB, CCC)"
+                                        "BBB = (DDD, EEE)"
+                                        "CCC = (ZZZ, GGG)"
+                                        "DDD = (DDD, DDD)"
+                                        "EEE = (EEE, EEE)"
+                                        "GGG = (GGG, GGG)"
+                                        "ZZZ = (ZZZ, ZZZ)"]) 2))
+    (test/is (= (core/procesar-tarea-1 ["LLR"
+                                        ""
+                                        "AAA = (BBB, BBB)"
+                                        "BBB = (AAA, ZZZ)"
+                                        "ZZZ = (ZZZ, ZZZ)"]) 6))))
