b1880be81dcbb27bdad76ea6700225993680a734
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-10 00:53:35 +0100

    Resuelta tarea 1 dia 9

--- day-9/src/day_9/core.clj
+++ day-9/src/day_9/core.clj
@@ -1,7 +1,59 @@
 (ns day-9.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as st]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; obtener-diferencias :: [Int] -> [Int]
+(defn obtener-diferencias [lista] 
+  (loop [indice 0
+         lista-salida []]
+    (if (= indice (- (count lista) 1))
+      lista-salida
+      (let [nuevo-indice (+ indice 1)
+            valor1 (get lista indice)
+            valor2 (get lista (+ indice 1))
+            diferencia (- valor2 valor1)
+            nueva-lista-salida (conj lista-salida diferencia)] 
+        (recur nuevo-indice nueva-lista-salida)))))
+
+; zeros :: Int -> [0 .. 0]
+(defn zeros [numero-ceros]
+  (-> (repeat numero-ceros 0)
+      (into [])))
+
+; obtener-arbol-diferencias :: [Int] -> [[Int]]
+(defn obtener-arbol-diferencias [lista]
+  (loop [arbol-diferencias [lista]] 
+    (if (= (last arbol-diferencias) (zeros (count (last arbol-diferencias))))
+      arbol-diferencias
+      (let [lista-examinada (last arbol-diferencias)
+            lista-diferencias (obtener-diferencias lista-examinada)
+            nuevo-arbol-diferencias (conj arbol-diferencias lista-diferencias)]
+        (recur nuevo-arbol-diferencias)))))
+
+; obtener-siguiente-numero :: [[Int]] -> Int
+(defn obtener-siguiente-numero [lista-de-listas]
+  (loop [indice (count lista-de-listas)
+         acumulador 0]
+    (if (= indice 0)
+      acumulador
+      (let [lista (get lista-de-listas (- indice 1))
+            valor (last lista)]
+        (recur (- indice 1) (+ acumulador valor))))))
+
+; procesar-tarea-1 :: String -> Int
+(defn procesar-tarea-1 [cadena]
+  (let [lista-numeros (->>
+                       (-> cadena
+                           (st/split #" "))
+                       (map #(Integer/parseInt %))
+                       (into []))
+        arbol-diferencias (obtener-arbol-diferencias lista-numeros)]
+    (obtener-siguiente-numero arbol-diferencias)))
+
+(defn -main [& args]
+  (let [entrada-tareas (->> "./resources/input.lst"
+                            (slurp)
+                            (st/split-lines))
+        salida-tarea-1 (map procesar-tarea-1 entrada-tareas) 
+        tarea-1 (reduce + 0 salida-tarea-1)]
+    (println tarea-1)))
--- day-9/test/day_9/core_test.clj
+++ day-9/test/day_9/core_test.clj
@@ -1,7 +1,17 @@
 (ns day-9.core-test
-  (:require [clojure.test :refer :all]
-            [day-9.core :refer :all]))
+  (:require [clojure.test :as test]
+            [day-9.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(test/deftest day-9-test
+  (test/testing "Probando obtener-diferencias"
+    (test/is (= (core/obtener-diferencias [1 3 6 10 15 21]) [2 3 4 5 6]))
+    (test/is (= (core/obtener-diferencias [2 3 4 5 6]) [1 1 1 1]))
+    (test/is (= (core/obtener-diferencias [1 1 1 1]) [0 0 0])))
+  (test/testing "Probando obtener-arbol-diferencias"
+    (test/is (= (core/obtener-arbol-diferencias [0 3 6 9 12 15]) [[0 3 6 9 12 15] [3 3 3 3 3] [0 0 0 0]]))
+    (test/is (= (core/obtener-arbol-diferencias [1 3 6 10 15 21]) [[1 3 6 10 15 21] [2 3 4 5 6] [1 1 1 1] [0 0 0]]))
+    (test/is (= (core/obtener-arbol-diferencias [10 13 16 21 30 45 68]) [[10 13 16 21 30 45 68] [3 3 5 9 15 23] [0 2 4 6 8] [2 2 2 2] [0 0 0]])))
+  (test/testing "Probando obtener-siguiente-numero"
+    (test/is (= (core/obtener-siguiente-numero [[0 3 6 9 12 15] [3 3 3 3 3] [0 0 0 0]]) 18))
+    (test/is (= (core/obtener-siguiente-numero [[1 3 6 10 15 21] [2 3 4 5 6] [1 1 1 1] [0 0 0]]) 28))
+    (test/is (= (core/obtener-siguiente-numero [[10 13 16 21 30 45] [3 3 5 9 15] [0 2 4 6] [2 2 2] [0 0]]) 68))))
