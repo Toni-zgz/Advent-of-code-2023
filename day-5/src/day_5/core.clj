41c5f1e7a8f43bb71b37a05411ee47388b61488f
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-10 01:09:28 +0100

    Resuelta tarea 1 dia 5

--- day-5/src/day_5/core.clj
+++ day-5/src/day_5/core.clj
@@ -1,7 +1,107 @@
 (ns day-5.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as st]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; convertir :: [[Int]] -> Int
+(defn convertir [tabla-conversion valor]
+  (loop [tabla tabla-conversion]
+    (if (= tabla '())
+      valor
+      (let [fila-tabla (first tabla)
+            inicio-fuente (second fila-tabla)
+            inicio-destino (first fila-tabla)
+            rango (nth fila-tabla 2)
+            final-fuente (+ inicio-fuente rango)]
+        (if (and (>= valor inicio-fuente)
+                 (<= valor final-fuente))
+          (+ inicio-destino (- valor inicio-fuente))
+          (recur (rest tabla)))))))
+
+; obtener-datos :: String -> Dictionary
+(defn obtener-datos [cadena]
+  (let [lista (st/split cadena #"seeds:|\nseed-to-soil map:|\nsoil-to-fertilizer map:|\nfertilizer-to-water map:|\nwater-to-light map:|\nlight-to-temperature map:|\ntemperature-to-humidity map:|\nhumidity-to-location map:")
+        tabla-semillas (->>
+                        (-> (second lista)
+                            (st/trim)
+                            (st/split-lines)
+                            (first)
+                            (st/split #" "))
+                        (map #(bigint %)))
+        tabla-semillas-a-suelo (->>
+                                (-> (nth lista 2)
+                                    (st/trim)
+                                    (st/split-lines))
+                                (map #(st/split % #" "))
+                                (map #(map (fn [elt] (bigint elt)) %)))
+        tabla-suelo-a-fertilizante (->>
+                                    (-> (nth lista 3)
+                                        (st/trim)
+                                        (st/split-lines))
+                                    (map #(st/split % #" "))
+                                    (map #(map (fn [elt] (bigint elt)) %)))
+        tabla-fertilizante-a-agua (->>
+                                   (-> (nth lista 4)
+                                       (st/trim)
+                                       (st/split-lines))
+                                   (map #(st/split % #" "))
+                                   (map #(map (fn [elt] (bigint elt)) %)))
+        tabla-agua-a-luz (->>
+                          (-> (nth lista 5)
+                              (st/trim)
+                              (st/split-lines))
+                          (map #(st/split % #" "))
+                          (map #(map (fn [elt] (bigint elt)) %)))
+        tabla-luz-a-temperatura (->>
+                                 (-> (nth lista 6)
+                                     (st/trim)
+                                     (st/split-lines))
+                                 (map #(st/split % #" "))
+                                 (map #(map (fn [elt] (bigint elt)) %)))
+        tabla-temperatura-a-humedad (->>
+                                     (-> (nth lista 7)
+                                         (st/trim)
+                                         (st/split-lines))
+                                     (map #(st/split % #" "))
+                                     (map #(map (fn [elt] (bigint elt)) %)))
+        tabla-humedad-a-localizacion (->>
+                                      (-> (nth lista 8)
+                                          (st/trim)
+                                          (st/split-lines))
+                                      (map #(st/split % #" "))
+                                      (map #(map (fn [elt] (bigint elt)) %)))]
+    {:seeds tabla-semillas
+     :seed-to-soil tabla-semillas-a-suelo
+     :soil-to-fertilizer tabla-suelo-a-fertilizante
+     :fertilizer-to-water tabla-fertilizante-a-agua
+     :water-to-light tabla-agua-a-luz
+     :light-to-temperature tabla-luz-a-temperatura
+     :temperature-to-humidity tabla-temperatura-a-humedad
+     :humidity-to-location tabla-humedad-a-localizacion}))
+
+; procesar-tarea-1 :: [String] -> [Int]
+(defn procesar-tarea-1 [texto]
+  (let [tablas (obtener-datos texto)
+        tabla-semillas (:seeds tablas)
+        tabla-semillas-a-suelo (:seed-to-soil tablas)
+        tabla-suelo-a-fertilizante (:soil-to-fertilizer tablas)
+        tabla-fertilizante-a-agua (:fertilizer-to-water tablas)
+        tabla-agua-a-luz (:water-to-light tablas)
+        tabla-luz-a-temperatura (:light-to-temperature tablas)
+        tabla-temperatura-a-humedad (:temperature-to-humidity tablas)
+        tabla-humedad-a-localizacion (:humidity-to-location tablas)]
+    (->> tabla-semillas
+         (map #(convertir tabla-semillas-a-suelo %))
+         (map #(convertir tabla-suelo-a-fertilizante %))
+         (map #(convertir tabla-fertilizante-a-agua %))
+         (map #(convertir tabla-agua-a-luz %))
+         (map #(convertir tabla-luz-a-temperatura %))
+         (map #(convertir tabla-temperatura-a-humedad %))
+         (map #(convertir tabla-humedad-a-localizacion %)))))
+
+(defn -main [& args]
+  (let [entrada-tareas (->> "./resources/input.lst"
+                            (slurp))
+        salida-tarea-1 (procesar-tarea-1 entrada-tareas)
+        tarea-1 (->> salida-tarea-1
+                     (reduce min))]
+    (println tarea-1)))
--- day-5/test/day_5/core_test.clj
+++ day-5/test/day_5/core_test.clj
@@ -1,7 +1,71 @@
 (ns day-5.core-test
-  (:require [clojure.test :refer :all]
-            [day-5.core :refer :all]))
+  (:require [clojure.test :as test]
+            [day-5.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def cadena "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4")
+
+(def datos {:seeds '(79 14 55 13)
+            :seed-to-soil '((50 98 2)
+                            (52 50 48))
+            :soil-to-fertilizer '((0 15 37)
+                                  (37 52 2)
+                                  (39 0 15))
+            :fertilizer-to-water '((49 53 8)
+                                   (0 11 42)
+                                   (42 0 7)
+                                   (57 7 4))
+            :water-to-light '((88 18 7)
+                              (18 25 70))
+            :light-to-temperature '((45 77 23)
+                                    (81 45 19)
+                                    (68 64 13))
+            :temperature-to-humidity '((0 69 1)
+                                       (1 0 69))
+            :humidity-to-location '((60 56 37)
+                                    (56 93 4))})
+
+(def seed->soil '((50 98 2)
+                  (52 50 48)))
+
+(test/deftest day-5-test
+  (test/testing "probando convertir"
+    (test/is (= (core/convertir seed->soil 79) 81))
+    (test/is (= (core/convertir seed->soil 14) 14))
+    (test/is (= (core/convertir seed->soil 55) 57))
+    (test/is (= (core/convertir seed->soil 98) 50)))
+  (test/testing "probando obtener-datos"
+    (test/is (= (core/obtener-datos cadena) datos)))
+  (test/testing "probando procesar-tarea-1"
+    (test/is (= (core/procesar-tarea-1 cadena) '(82 43 86 35)))))
