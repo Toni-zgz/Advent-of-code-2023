42c05076c59227d39c7480768b8a0c493301adc3
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-16 22:50:05 +0100

    Resuelta tarea-2 dia 15

--- day-15/src/day_15/core.clj
+++ day-15/src/day_15/core.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require [clojure.string :as st]))
 
+; obtener-hash :: String -> Int
 (defn obtener-hash [cadena]
   (reduce (fn [acc elt]
            (if (= elt \space)
@@ -13,14 +14,95 @@
                 (rem 256))))
           0 cadena))
 
+; obtener-puntuacion :: {Int, [[String Int]]} -> [Int]
+(defn obtener-puntuacion [mapa]
+  (map (fn [elt1]
+         (loop [valor 0
+                lista-entrada (second elt1)
+                slot 1] 
+           (if (= lista-entrada [])
+             valor
+             (let [elt2 (first lista-entrada)
+                   caja (+ (first elt1) 1)
+                   longitud-focal (second elt2)
+                   calculo (* caja slot longitud-focal)
+                   nuevo-valor (+ valor calculo)
+                   nueva-lista-entrada (rest lista-entrada)]
+             (recur nuevo-valor nueva-lista-entrada (+ slot 1))))))
+       mapa))
+
+; actualizar-valor :: [[String Int]] -> [String Int] -> [[String Int]]
+(defn actualizar-valor [lista-de-listas lista]
+  (loop [lista-entrada lista-de-listas
+         actualizado? false
+         lista-salida []]
+    (cond (and (= lista-entrada []) 
+               actualizado?) lista-salida  
+          (= lista-entrada []) (conj lista-salida lista)
+          (nil? lista-de-listas) [lista]
+          :else (let [elt (first lista-entrada)
+                      identificador (first elt)
+                      ident-buscar (first lista)
+                      valor-a-actualizar (second lista)
+                      nuevo-actualizado? (if (= identificador ident-buscar)
+                                           true
+                                           actualizado?)
+                      nuevo-valor (if (= identificador ident-buscar)
+                                    [identificador valor-a-actualizar]
+                                    elt)
+                      nueva-lista-salida (conj lista-salida nuevo-valor)]
+                  (recur (rest lista-entrada) nuevo-actualizado? nueva-lista-salida)))))
+
+; eliminar-identificador :: [[String Int]] -> String -> [[String Int]]
+(defn eliminar-identificador [lista-de-listas identificador]
+  (filter (fn [elt]
+            (not (= (first elt) identificador)))
+          lista-de-listas))
+
+; lista-codigo->mapa :: [String] -> {Int, [[String Int]]}
+(defn lista-codigo->mapa [lista]
+  (loop [mapa-salida {}
+         lista-entrada lista]
+    (if (= lista-entrada [])
+      mapa-salida
+      (let [elemento (first lista-entrada)
+            codigo (if (st/includes? elemento "=")
+                     (let [lista-codigo (st/split elemento #"=")
+                           identificador (first lista-codigo)
+                           valor (Long/parseLong (second lista-codigo))]
+                       [identificador valor])
+                     (st/split elemento #"-"))
+            identificador (first codigo)
+            clave (obtener-hash identificador)
+            valor (mapa-salida clave)
+            nuevo-valor (if (st/includes? elemento "=")
+                         (actualizar-valor valor codigo)
+                         (eliminar-identificador valor identificador))
+            nuevo-mapa-salida (if (= nuevo-valor '())
+                                (dissoc mapa-salida clave)
+                                (assoc mapa-salida clave nuevo-valor))
+            nueva-lista-entrada (rest lista-entrada)]
+        (recur nuevo-mapa-salida nueva-lista-entrada)))))
+
+; procesar-tarea-2 :: [String] -> [Int]
+(defn procesar-tarea-2 [lista]
+  (-> lista
+      (lista-codigo->mapa)
+      (obtener-puntuacion)))
+
+; procesar-tarea-1 :: [String] -> [Int]
 (defn procesar-tarea-1 [lista]
   (map obtener-hash lista))
 
 (defn -main [& args]
   (let [entrada-tareas (-> "./resources/input.lst"
-                            (slurp)
-                            (st/split #","))
-        salida-tarea-1 (procesar-tarea-1 entrada-tareas) 
+                           (slurp)
+                           (st/split #","))
+        salida-tarea-1 (procesar-tarea-1 entrada-tareas)
+        salida-tarea-2 (procesar-tarea-2 entrada-tareas)
         tarea-1 (->> salida-tarea-1
+                     (reduce + 0))
+        tarea-2 (->> salida-tarea-2
                      (reduce + 0))]
-         (println tarea-1)))
+    (println tarea-1)
+    (println tarea-2)))
--- day-15/test/day_15/core_test.clj
+++ day-15/test/day_15/core_test.clj
@@ -4,7 +4,20 @@
 
 (test/deftest day-15-test
   (test/testing "probando obtener-hash"
-    (test/is (= (core/obtener-hash "HASH") 52)
-             (= (core/obtener-hash "rn=1") 30)))
+    (test/is (= (core/obtener-hash "HASH") 52))
+    (test/is (= (core/obtener-hash "rn=1") 30)))
   (test/testing "probando procesar-tarea-1"
-    (test/is (= (core/procesar-tarea-1 ["rn=1" "cm-" "qp=3" "cm=2" "qp-" "pc=4" "ot=9" "ab=5" "pc-" "pc=6" "ot=7"]) [30 253 97 47 14 180 9 197 48 214 231]))))
+    (test/is (= (core/procesar-tarea-1 ["rn=1" "cm-" "qp=3" "cm=2" "qp-" "pc=4" "ot=9" "ab=5" "pc-" "pc=6" "ot=7"]) [30 253 97 47 14 180 9 197 48 214 231])))
+  (test/testing "obtener-puntuacion"
+    (test/is (= (core/obtener-puntuacion {0 [["rn" 1] ["cm" 2]]
+                                          3 [["ot" 7] ["ab" 5] ["pc" 6]]}) [5 140])))
+  (test/testing "probando eliminar-identificador"
+    (test/is (= (core/eliminar-identificador [["rn" 1] ["cm" 2]] "rn") [["cm" 2]]))
+    (test/is (= (core/eliminar-identificador [["rn" 1] ["cm" 2]] "cm") [["rn" 1]]))
+    (test/is (= (core/eliminar-identificador [["rn" 1] ["cm" 2]] "qp") [["rn" 1] ["cm" 2]])))
+  (test/testing "probando actualizar-valor"
+    (test/is (= (core/actualizar-valor [["ot" 7] ["ab" 5] ["pc" 6]] ["ab" 3]) [["ot" 7] ["ab" 3] ["pc" 6]]))
+    (test/is (= (core/actualizar-valor [["ot" 7] ["ab" 5] ["pc" 6]] ["rn" 2]) [["ot" 7] ["ab" 5] ["pc" 6]["rn" 2]])))
+  (test/testing "probando lista-codigo->mapa"
+    (test/is (= (core/lista-codigo->mapa ["rn=1" "cm-" "qp=3" "cm=2" "qp-" "pc=4" "ot=9" "ab=5" "pc-" "pc=6" "ot=7"]) {0 [["rn" 1] ["cm" 2]]
+                                                                                                                       3 [["ot" 7] ["ab" 5] ["pc" 6]]}))))
