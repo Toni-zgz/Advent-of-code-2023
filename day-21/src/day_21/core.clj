c3b800f2808f398982007ac17be0f1345e1797e2
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-12-23 23:02:07 +0100

    creada funcion obtener-coordenadas

--- day-21/src/day_21/core.clj
+++ day-21/src/day_21/core.clj
@@ -1,7 +1,41 @@
 (ns day-21.core
-  (:gen-class))
+  (:gen-class)
+  (:require clojure.string :as st))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; get-2d :: [A] -> Int -> Int -> A
+(defn get-2d [lista nfila ncolumna]
+  (-> lista
+       (get nfila)
+       (get ncolumna)))
+
+; set-2d :: [A] -> Int -> Int -> A -> [A]
+(defn set-2d [lista nfila ncolumna valor]
+  (let [fila (get lista nfila)
+        nueva-fila (assoc fila ncolumna valor)]
+    (assoc lista nfila nueva-fila)))
+
+; obtener-coordenadas :: [String] -> [[Int Int]]
+(defn obtener-coordenadas [lista-cadenas]
+  (let [alto (count lista-cadenas)
+        ancho (-> (first lista-cadenas)
+                  (count))
+        rango-columnas (range 0 ancho)
+        rango-filas (range 0 alto)
+        lista-coordenadas (for [fila rango-filas
+                                columna rango-columnas]
+                            [fila columna])
+        es-una-o? (fn [elt]
+                    (let [x (first elt)
+                          y (second elt)
+                          valor (get-2d lista-cadenas x y)]
+                      (= valor \O)))]
+    (filter es-una-o? lista-coordenadas)))
+
+(defn -main [& args]
+  (let [entrada-tareas (->> "./resources/input.lst"
+                            (slurp)
+                            (st/split-lines))
+        num-pasos 64
+        salida-tarea-1 (procesar-tarea-1 entrada-tareas num-pasos) 
+        tarea-1 (reduce + 0 salida-tarea-1)]
+    (println tarea-1)))
--- day-21/test/day_21/core_test.clj
+++ day-21/test/day_21/core_test.clj
@@ -2,6 +2,37 @@
   (:require [clojure.test :refer :all]
             [day-21.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def tablero-1 ["..........."
+                ".....###.#."
+                ".###.##..#."
+                "..#.#.O.#.."
+                "...O#O#...."
+                ".##.OS####."
+                ".##O.#...#."
+                "....O..##.."
+                ".##.#.####."
+                ".##..##.##."
+                "..........."])
+
+(def tablero-2 ["..........."
+                ".....###.#."
+                ".###.##.O#."
+                ".O#O#O.O#.."
+                "O.O.#.#.O.."
+                ".##O.O####."
+                ".##.O#O..#."
+                ".O.O.O.##.."
+                ".##.#.####."
+                ".##O.##.##."
+                "..........."])
+
+(deftest day-21-test
+  (testing "probando get-2d"
+    (is (= (get-2d tablero-1 2 3) 
+           \#))
+    (is (= (get-2d tablero-1 4 5) 
+           \O)))
+  (testing "Probando obtener-coordenadas"
+    (is (= (obtener-coordenadas tablero-1) 
+           [[3 6] [4 3] [4 5] [5 4] [6 3] [7 4]]))))
+  
