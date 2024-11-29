2dd21cceb426d61bab7b11178c77c8c57f6d09d4
Toni-zgz <ajaczgz@yahoo.es>
2024-11-29 19:11:31 +0000

    Day 3 - Resuelta tarea 2.

--- day-3/src/day_3/core.clj
+++ day-3/src/day_3/core.clj
@@ -2,15 +2,20 @@
   (:gen-class)
   (:require [clojure.string :as st]))
 
-; simbolo? :: Char -> True
-(defn simbolo? [caracter]
-  ; Es un simbolo si no es un digito ni un punto.
-  (cond (Character/isDigit caracter) false
-        (= \. caracter) false
-        :else true))
+;;;;;;;;;;;;;;;;;;;;; Funciones internas ;;;;;;;;;;;;;;;;;;;;;;
 
-; obtener-coord-simbolos :: [[Char]] -> [(Int Int)]
-(defn obtener-coord-simbolos [tablero]
+; simbolo? :: Char -> True
+(defn- simbolo? [caracter]
+  ; Es un simbolo si no es un digito ni un punto.
+  (and (not (Character/isDigit caracter))
+        (not (= \. caracter))))
+
+; asterisco? :: Char -> True
+(defn- asterisco? [caracter]
+       (= \* caracter))
+
+; obtener-coords :: fn -> [[Char]] -> [(Int Int)]
+(defn- obtener-coords [pred tablero]
   (loop [num-fila 0
          coords []]
     (if (= num-fila (count tablero))
@@ -26,14 +31,14 @@
                             (if (= num-columna (count fila))
                               elems
                               (let [nuevo-num-columna (+ num-columna 1)
-                                    coords-simbolo (if (simbolo? (nth fila num-columna))
+                                    coords-simbolo (if (pred (nth fila num-columna))
                                                      (conj elems (list num-fila num-columna))
                                                      elems)]
                                 (recur nuevo-num-columna coords-simbolo))))] 
         (recur nuevo-num-fila (conj coords nuevas-coord))))))
 
 ; dentro-tablero? :: Int -> Int -> Int -> Int -> Bool
-(defn dentro-tablero? [fila columna max-fila max-columna]
+(defn- dentro-tablero? [fila columna max-fila max-columna]
   (cond (= fila 0) false
         (= columna 0) false
         (= fila max-fila) false
@@ -41,7 +46,7 @@
         :else true))
 
 ; obtener-coords-adyacentes-casilla :: [[Char]] -> (Int Int) -> [(Int Int)]
-(defn obtener-coords-adyacentes-casilla [tablero casilla] 
+(defn- obtener-coords-adyacentes-casilla [tablero casilla] 
   (let [fila (first casilla)
         columna (second casilla)
         max-fila (count tablero)
@@ -72,22 +77,30 @@
                   '())] 
     (list sup-izq sup-cen sup-der cen-izq cen-der inf-izq inf-cen inf-der)))
 
-; obtener-coords-adyacentes-numero :: [[Char]] -> [(Int Int)] -> [(Int Int)]
-(defn obtener-coords-adyacentes-con-digito [tablero lista-coords]
+; obtener-coords-adyacentes-con-digito :: [[Char]] -> [(Int Int)] -> ([A] -> [A] -> [A] | [(A)]) -> [(Int Int)] | [((Int Int))]
+(defn- obtener-coords-adyacentes-con-digito [tablero lista-coords funcion-union]
   (loop [lista-entrada lista-coords
          salida []]
     (if (= lista-entrada [])
-      (->> salida
-           (flatten)
-           (partition 2))
+      salida
       (let [casilla (first lista-entrada)
             nueva-lista-entrada (rest lista-entrada)
             nuevos-elems (obtener-coords-adyacentes-casilla tablero casilla)
-            nueva-salida (conj salida nuevos-elems)]
+            nueva-salida (funcion-union salida nuevos-elems)]
         (recur nueva-lista-entrada nueva-salida)))))
 
-; obtener-coord-digitos :: [[Char]] -> [(Int Int)] -> [(Int Int)]
-(defn obtener-coord-digitos [tablero lista-coords]
+;;;;;;;;;;;;;;;;;;;;; Funciones tarea 1 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; obtener-coord-simbolos :: [[Char]] -> [(Int Int)]
+(defn obtener-coord-simbolos [tablero]
+  (obtener-coords simbolo? tablero))
+
+; obtener-coords-adyacentes-con-digito-simbolos :: [[Char]] -> [(Int Int)] -> [(Int Int)]
+(defn obtener-coords-adyacentes-con-digito-simbolos [tablero lista-coords]
+  (obtener-coords-adyacentes-con-digito tablero lista-coords concat))
+
+; obtener-coord-digitos-simbolos :: [[Char]] -> [(Int Int)] -> [(Int Int)]
+(defn obtener-coord-digitos-simbolos [tablero lista-coords]
   (let [numero? (fn [casilla]
                   (let [fila (first casilla)
                         columna (second casilla)
@@ -97,8 +110,8 @@
                     (Character/isDigit elt)))]
     (filter numero? lista-coords)))
 
-; obtener-coord-numeros :: [[Char]] -> [(Int Int)] -> [((Int Int) (Int Int))]
-(defn obtener-coord-numeros [tablero lista-coords]
+; obtener-coord-numeros-simbolos :: [[Char]] -> [(Int Int)] -> [((Int Int) (Int Int))]
+(defn obtener-coord-numeros-simbolos [tablero lista-coords]
   (let [funcion-mapeo (fn [elt]
                         (let [casilla elt
                               fila (first casilla)
@@ -163,29 +176,126 @@
 (defn lista-cadenas-numeros->lista-numeros [lista-cadenas]
   (map read-string lista-cadenas))
 
-; procesar-tarea-1 :: [[Char]] -> [Int]
+; procesar-tarea-1 :: [[Char]] -> Int
 (defn procesar-tarea-1 [tablero]
   (->> tablero
        (obtener-coord-simbolos)
-       (obtener-coords-adyacentes-con-digito tablero)
-       (obtener-coord-digitos tablero)
-       (obtener-coord-numeros tablero)
+       (obtener-coords-adyacentes-con-digito-simbolos tablero)
+       (obtener-coord-digitos-simbolos tablero)
+       (obtener-coord-numeros-simbolos tablero)
        (quitar-duplicados)
        (obtener-cadenas-numeros tablero)
-       (lista-cadenas-numeros->lista-numeros)))
+       (lista-cadenas-numeros->lista-numeros) 
+       (reduce + 0)))
+
+;;;;;;;;;;;;;;;;;;;;; Funciones tarea 2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; obtener-coord-asteriscos :: [[Char]] -> [(Int Int)]
+(defn obtener-coord-asteriscos [tablero]
+  (obtener-coords asterisco? tablero))
+
+; obtener-coords-adyacentes-con-digito-asteriscos :: [[Char]] -> [(Int Int)] -> [((Int Int))]
+(defn obtener-coords-adyacentes-con-digito-asteriscos [tablero lista-coords]
+  (obtener-coords-adyacentes-con-digito tablero lista-coords conj))
+
+; obtener-coord-digitos-asteriscos :: [[Char]] -> [((Int Int))] -> [((Int Int))]
+(defn obtener-coord-digitos-asteriscos [tablero lista-coords]
+  (let [numero? (fn [casilla]
+                  (let [fila (first casilla)
+                        columna (second casilla)
+                        elt (-> tablero
+                                 (nth fila)
+                                 (nth columna))]
+                    (Character/isDigit elt)))
+        funcion-mapeo (fn [elt]
+                        (filter numero? elt))]
+    (map funcion-mapeo lista-coords)))
+
+; obtener-coord-numeros-asteriscos :: [[Char]] -> [((Int Int))] -> [(((Int Int) (Int Int)))]
+(defn obtener-coord-numeros-asteriscos [tablero lista-coords]
+  (let [funcion-mapeo-interna (fn [elt]
+                        (let [casilla elt
+                              fila (first casilla)
+                              columna (second casilla)
+                              columna-maxima (-> tablero
+                                                 (first)
+                                                 (count)
+                                                 (- 1))
+            ; Tengo que inicializar la variable caracter a \0 porque sino me da un error
+            ; java.lang.IllegalArgumentException: No matching method isDigit found taking 1 args,
+            ; dicha inicialización no afecta al programa porque se supone que el caracter inicial es un digito.
+                              datos-izq (loop [indice columna
+                                               caracter \0]
+                                          (cond (not (Character/isDigit caracter)) (list fila (+ indice 1))
+                                                (= indice 0) (list fila 0)
+                                                :else (let [nuevo-indice (- indice 1)
+                                                            nuevo-caracter (-> tablero
+                                                                               (nth fila)
+                                                                               (nth nuevo-indice))]
+                                                        (recur nuevo-indice nuevo-caracter))))
+            ; Tengo que inicializar la variable caracter a \0 porque sino me da un error
+            ; java.lang.IllegalArgumentException: No matching method isDigit found taking 1 args,
+            ; dicha inicialización no afecta al programa porque se supone que el caracter inicial es un digito.
+                              datos-dcha (loop [indice columna
+                                                caracter \0]
+                                           (cond (not (Character/isDigit caracter)) (list fila (- indice 1))
+                                                 (= indice columna-maxima) (list fila columna-maxima)
+                                                 :else (let [nuevo-indice (+ indice 1)
+                                                             nuevo-caracter (-> tablero
+                                                                                (nth fila)
+                                                                                (nth nuevo-indice))]
+                                                         (recur nuevo-indice nuevo-caracter))))]
+                          (list datos-izq datos-dcha)))
+        funcion-mapeo-externa (fn [elt] 
+                                (map funcion-mapeo-interna elt))]
+      (map funcion-mapeo-externa lista-coords)))
+
+; quitar-duplicados-asteriscos :: [(A)] -> [(A)]
+(defn quitar-duplicados-asteriscos [lista-entrada]
+  (map quitar-duplicados lista-entrada))
+
+; obtener-coord-digitos-engranajes :: [((Int Int))] -> [((Int Int))]
+(defn obtener-coord-digitos-engranajes [lista-coords]
+  (let [engranaje? (fn [elt]
+                        (-> elt
+                          (count)
+                          (= 2)))]
+    (filter engranaje? lista-coords)))
+
+; obtener-cadenas-numeros-engranajes :: [[Char]] -> [(((Int Int) (Int Int)))] -> [(String)]
+(defn obtener-cadenas-numeros-engranajes [tablero lista-limites]
+  (map #(obtener-cadenas-numeros tablero %) lista-limites))
+
+; lista-cadenas-numeros-engranajes->lista-numeros-engranajes :: [(String)] -> [(Int)]
+(defn lista-cadenas-numeros-engranajes->lista-numeros-engranajes [lista-cadenas]
+  (map #(map read-string %) lista-cadenas))
+
+; calcular-potencia-engranaje :: [(Int)] -> [Int]
+(defn calcular-potencia-engranaje [lista-numeros]
+  (map #(reduce * 1 %) lista-numeros))
+
+; procesar-tarea-2 :: [[Char]] -> Int
+ (defn procesar-tarea-2 [tablero]
+  (->> tablero
+       (obtener-coord-asteriscos)
+       (obtener-coords-adyacentes-con-digito-asteriscos tablero)
+       (obtener-coord-digitos-asteriscos tablero)
+       (obtener-coord-numeros-asteriscos tablero)
+       (quitar-duplicados-asteriscos)
+       (obtener-coord-digitos-engranajes)
+       (obtener-cadenas-numeros-engranajes tablero)
+       (lista-cadenas-numeros-engranajes->lista-numeros-engranajes)
+       (calcular-potencia-engranaje)
+       (reduce + 0)))
+
+;;;;;;;;;;;;;;;;;;;;; Programa principal ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn -main [& args]
   (let [entrada-tareas (->> "./resources/input.lst"
-                            (slurp)
-                            (st/split-lines)
-                            (map seq))
+                            (slurp)          ; Unit -> String
+                            (st/split-lines) ; String -> [String]
+                            (map seq))       ; [String] -> [[Char]]
         salida-tarea-1 (procesar-tarea-1 entrada-tareas)
-        ;salida-tarea-2 (map procesar-tarea-2 entrada-tareas)
-        tarea-1 (->> salida-tarea-1
-                     (reduce + 0))
-        ;tarea-2 (->> salida-tarea-2
-        ;             (reduce + 0))
-        ]
-    (println tarea-1)
-    ;(println tarea-2)
-    ))
+        salida-tarea-2 (procesar-tarea-2 entrada-tareas)]
+    (println salida-tarea-1)
+    (println salida-tarea-2)))
--- day-3/test/day_3/core_test.clj
+++ day-3/test/day_3/core_test.clj
@@ -15,42 +15,88 @@
 
 (def coord-simbolos '[(1 3) (3 6) (4 3) (5 5) (8 3) (8 5)])
 
-(def coord-adyacentes '[(0 2) (0 3) (0 4) (1 2) (1 4) (2 2) (2 3) (2 4)
-                        (2 5) (2 6) (2 7) (3 5) (3 7) (4 5) (4 6) (4 7)
-                        (3 2) (3 3) (3 4) (4 2) (4 4) (5 2) (5 3) (5 4)
-                        (4 4) (4 5) (4 6) (5 4) (5 6) (6 4) (6 5) (6 6)
-                        (7 2) (7 3) (7 4) (8 2) (8 4) (9 2) (9 3) (9 4)
-                        (7 4) (7 5) (7 6) (8 4) (8 6) (9 4) (9 5) (9 6)])
+(def coord-adyacentes-simbolos '[(0 2) (0 3) (0 4) (1 2) (1 4) (2 2) (2 3) (2 4)
+                                 (2 5) (2 6) (2 7) (3 5) (3 7) (4 5) (4 6) (4 7)
+                                 (3 2) (3 3) (3 4) (4 2) (4 4) (5 2) (5 3) (5 4)
+                                 (4 4) (4 5) (4 6) (5 4) (5 6) (6 4) (6 5) (6 6)
+                                 (7 2) (7 3) (7 4) (8 2) (8 4) (9 2) (9 3) (9 4)
+                                 (7 4) (7 5) (7 6) (8 4) (8 6) (9 4) (9 5) (9 6)])
 
-(def coord-digitos '[(0 2) (2 2) (2 3) (2 6) (2 7) (4 2) (6 4) (9 2) (9 3) (7 6) (9 5) (9 6)])
+(def coord-digitos-simbolos '[(0 2) (2 2) (2 3) (2 6) (2 7) (4 2) (6 4) (9 2) (9 3) (7 6) (9 5) (9 6)])
 
-(def coord-numeros '[((0 0) (0 2)) ((2 2) (2 3)) ((2 2) (2 3)) ((2 6) (2 8)) ((2 6) (2 8)) ((4 0) (4 2))
-                     ((6 2) (6 4)) ((9 1) (9 3)) ((9 1) (9 3)) ((7 6) (7 8)) ((9 5) (9 7)) ((9 5) (9 7))])
+(def coord-numeros-simbolos '[((0 0) (0 2)) ((2 2) (2 3)) ((2 2) (2 3)) ((2 6) (2 8)) ((2 6) (2 8)) ((4 0) (4 2))
+                              ((6 2) (6 4)) ((9 1) (9 3)) ((9 1) (9 3)) ((7 6) (7 8)) ((9 5) (9 7)) ((9 5) (9 7))])
 
-(def sin-duplicados '[((0 0) (0 2)) ((2 2) (2 3)) ((2 6) (2 8)) ((4 0) (4 2))
-                     ((6 2) (6 4)) ((9 1) (9 3)) ((7 6) (7 8)) ((9 5) (9 7))])
+(def sin-duplicados-simbolos '[((0 0) (0 2)) ((2 2) (2 3)) ((2 6) (2 8)) ((4 0) (4 2))
+                               ((6 2) (6 4)) ((9 1) (9 3)) ((7 6) (7 8)) ((9 5) (9 7))])
 
-(def lista-cadenas '("467" "35" "633" "617" "592" "664" "755" "598"))
+(def lista-cadenas-simbolos '("467" "35" "633" "617" "592" "664" "755" "598"))
 
-(def lista-numeros '[467 35 633 617 592 664 755 598])
+(def lista-numeros-simbolos '[467 35 633 617 592 664 755 598])
 
-(test/deftest day-3-test
+(test/deftest tarea-1-test
   (test/testing "Probando la funcion obtener-coord-simbolos"
     (test/is (= (core/obtener-coord-simbolos inicial) coord-simbolos)))
-  (test/testing "Probando la funcion obtener-coord-adyacentes"
-    (test/is (= (core/obtener-coords-adyacentes-con-digito inicial coord-simbolos) coord-adyacentes)))
-  (test/testing "Probando la funcion obtener-coord-digitos"
-    (test/is (= (core/obtener-coord-digitos inicial coord-adyacentes) coord-digitos)))
-  (test/testing "Probando la funcion obtener-coord-numeros"
-    (test/is (= (core/obtener-coord-numeros inicial coord-digitos) coord-numeros)))
+  (test/testing "Probando la funcion obtener-coord-adyacentes-con-digito-simbolos"
+    (test/is (= (core/obtener-coords-adyacentes-con-digito-simbolos inicial coord-simbolos) coord-adyacentes-simbolos)))
+  (test/testing "Probando la funcion obtener-coord-digitos-simbolos"
+    (test/is (= (core/obtener-coord-digitos-simbolos inicial coord-adyacentes-simbolos) coord-digitos-simbolos)))
+  (test/testing "Probando la funcion obtener-coord-numeros-simbolos"
+    (test/is (= (core/obtener-coord-numeros-simbolos inicial coord-digitos-simbolos) coord-numeros-simbolos)))
   (test/testing "Probando la funcion quitar-duplicados"
-    (test/is (= (core/quitar-duplicados coord-numeros) sin-duplicados)))
+    (test/is (= (core/quitar-duplicados coord-numeros-simbolos) sin-duplicados-simbolos)))
   (test/testing "Probando la funcion obtener-cadenas-numeros"
-    (test/is (= (core/obtener-cadenas-numeros inicial sin-duplicados) lista-cadenas)))
+    (test/is (= (core/obtener-cadenas-numeros inicial sin-duplicados-simbolos) lista-cadenas-simbolos)))
   (test/testing "Probando la funcion lista-cadenas-numeros->lista-numeros"
-    (test/is (= (core/lista-cadenas-numeros->lista-numeros lista-cadenas) lista-numeros)))
+    (test/is (= (core/lista-cadenas-numeros->lista-numeros lista-cadenas-simbolos) lista-numeros-simbolos)))
   (test/testing "Probando la funcion procesar-tarea-1"
-    (test/is (= (->> inicial
-                 (core/procesar-tarea-1)
-                    (reduce + 0)) 
-                4361))))
+    (test/is (= (core/procesar-tarea-1 inicial) 4361))))
+
+(def coord-asteriscos '[(1 3) (4 3) (8 5)])
+
+(def coord-adyacentes-asteriscos '[((0 2) (0 3) (0 4) (1 2) (1 4) (2 2) (2 3) (2 4))
+   ((3 2) (3 3) (3 4) (4 2) (4 4) (5 2) (5 3) (5 4))
+   ((7 4) (7 5) (7 6) (8 4) (8 6) (9 4) (9 5) (9 6))])
+
+(def coord-digitos-asteriscos '[((0 2) (2 2) (2 3))
+                                ((4 2))
+                                ((7 6) (9 5) (9 6))])
+
+(def coord-numeros-asteriscos '[(((0 0) (0 2)) ((2 2) (2 3)) ((2 2) (2 3))) 
+                                (((4 0) (4 2)))
+                                (((7 6) (7 8)) ((9 5) (9 7)) ((9 5) (9 7)))])
+
+(def sin-duplicados-asteriscos '[(((0 0) (0 2)) ((2 2) (2 3)))
+                                 (((4 0) (4 2)))
+                                 (((7 6) (7 8)) ((9 5) (9 7)))])
+
+(def coord-numeros-engranajes '[(((0 0) (0 2)) ((2 2) (2 3)))
+                               (((7 6) (7 8)) ((9 5) (9 7)))])
+
+(def lista-cadenas-engranajes '(("467" "35") ("755" "598")))
+
+(def lista-numeros-engranajes '[(467 35) (755 598)])
+
+(def lista-potencia-engranajes '[16345 451490])
+
+(test/deftest tarea-2-test
+  (test/testing "Probando la funcion obtener-coord-asteriscos"
+    (test/is (= (core/obtener-coord-asteriscos inicial) coord-asteriscos)))
+  (test/testing "Probando la funcion obtener-coord-adyacentes-con-digito-asteriscos"
+    (test/is (= (core/obtener-coords-adyacentes-con-digito-asteriscos inicial coord-asteriscos) coord-adyacentes-asteriscos)))
+  (test/testing "Probando la funcion obtener-coord-digitos-asteriscos"
+    (test/is (= (core/obtener-coord-digitos-asteriscos inicial coord-adyacentes-asteriscos) coord-digitos-asteriscos)))
+  (test/testing "Probando la funcion obtener-coord-numeros-asteriscos"
+    (test/is (= (core/obtener-coord-numeros-asteriscos inicial coord-digitos-asteriscos) coord-numeros-asteriscos)))
+  (test/testing "Probando la funcion quitar-duplicados-asteriscos"
+    (test/is (= (core/quitar-duplicados-asteriscos coord-numeros-asteriscos) sin-duplicados-asteriscos)))
+  (test/testing "Probando la funcion obtener-coord-numeros-engranajes"
+    (test/is (= (core/obtener-coord-digitos-engranajes sin-duplicados-asteriscos) coord-numeros-engranajes)))
+  (test/testing "Probando la funcion obtener-cadenas-numeros-engranajes"
+    (test/is (= (core/obtener-cadenas-numeros-engranajes inicial coord-numeros-engranajes) lista-cadenas-engranajes)))
+  (test/testing "Probando la funcion lista-cadenas-numeros-engranajes->lista-numeros-engranajes"
+    (test/is (= (core/lista-cadenas-numeros-engranajes->lista-numeros-engranajes lista-cadenas-engranajes) lista-numeros-engranajes)))
+  (test/testing "Probando la funcion calcular-potencia-engranaje"
+    (test/is (= (core/calcular-potencia-engranaje lista-numeros-engranajes) lista-potencia-engranajes)))
+  (test/testing "Probando la funcion procesar-tarea-2"
+    (test/is (= (core/procesar-tarea-2 inicial) 467835))))
