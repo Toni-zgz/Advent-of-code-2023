8447853f20157c764f1a3d475f630e2bdb91a153
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2024-12-08 22:20:11 +0100

    Tarea 2 completada y optimizada pero por alguna razon la solución que da no es la correcta.

--- day-8/src/day_8/core.clj
+++ day-8/src/day_8/core.clj
@@ -2,29 +2,17 @@
   (:gen-class)
   (:require [clojure.string :as st]))
 
-; obtener-nuevo-ciclo :: {String, [String String]} -> [String] -> String -> [String]
+;;;;;;;;;;;;;;;;;;;;; Funciones comunes  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; obtener-nuevo-ciclo :: {String, [String String]} -> String -> String -> [String]
 (defn obtener-nuevo-nodo [nodos nodo direccion] 
   (if (= direccion "L")
-    (map #(-> (get nodos %)
-              (first)) nodo)
-    (map #(-> (get nodos %)
-              (second)) nodo)))
-
-; obtener-numero-ciclos-tarea-1 :: String -> {String, [String String]} -> Int
-(defn obtener-numero-ciclos-tarea-1 [cadena nodos]
-  (loop [numero-ciclos 0
-         indice-programa 0
-         nodo ["AAA"]]
-   (if (= nodo ["ZZZ"])
-     numero-ciclos
-     (let [nuevo-indice-programa (if (= indice-programa (- (count cadena) 1))
-                                   0
-                                   (+ indice-programa 1))
-           direccion (-> (get cadena indice-programa)
-                         (str))
-           nuevo-nodo (obtener-nuevo-nodo nodos nodo direccion)]
-       (println nuevo-nodo)
-       (recur (+ numero-ciclos 1) nuevo-indice-programa nuevo-nodo)))))
+    (->> nodo 
+        (get nodos)
+        (first))
+    (->> nodo
+        (get nodos)
+        (second))))
 
 ; obtener-datos :: [String] -> {String, [String String]}
 (defn obtener-datos [lista-cadenas]
@@ -44,14 +32,77 @@
             nuevos-datos (assoc datos clave valor)]
         (recur nuevos-datos (rest lista))))))
 
+; igual-a-ZZZ :: String -> Bool
+(defn igual-a-ZZZ [cadena]
+  (= cadena "ZZZ"))
+
+; termina-en-z? :: String -> Bool
+(defn termina-en-z? [cadena]
+  (st/ends-with? cadena "Z"))
+
+; gcd :: Int -> Int -> Int
+(defn gcd [a b]
+  (if (= b 0)
+  a
+  (gcd b (mod a b))))
+
+;;;;;;;;;;;;;;;;;;;;; Tarea 1 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; obtener-numero-ciclos-tarea-1 :: String -> {String, [String String]} -> String -> Fn -> Int
+(defn obtener-numero-ciclos-tarea-1 [cadena nodos valor-inicial valor-final]
+  (loop [numero-ciclos 0
+         indice-programa 0
+         nodo valor-inicial]
+   (if (valor-final nodo)
+     numero-ciclos
+     (let [nuevo-indice-programa (if (= indice-programa (- (count cadena) 1))
+                                   0
+                                   (+ indice-programa 1))
+           direccion (-> (get cadena indice-programa)
+                         (str))
+           nuevo-nodo (obtener-nuevo-nodo nodos nodo direccion)]
+       (recur (+ numero-ciclos 1) nuevo-indice-programa nuevo-nodo)))))
+
+; procesar-tarea-1 :: [String] -> Integer
 (defn procesar-tarea-1 [lista-filas]
   (let [cadena-programa (first lista-filas)
         dict-nodos (obtener-datos (drop 2 lista-filas))]
-    (obtener-numero-ciclos-tarea-1 cadena-programa dict-nodos)))
+    (obtener-numero-ciclos-tarea-1 cadena-programa dict-nodos "AAA" igual-a-ZZZ)))
 
-(defn -main [& args]
+;;;;;;;;;;;;;;;;;;;;; Tarea 2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; obtener-lista-inicial :: {String, [String String]} -> [String]
+(defn obtener-lista-inicial [dict]
+  (->> dict
+       (keys)
+       (filter #(st/ends-with? % "A"))))
+
+; obtener-numero-ciclos-tarea-2 :: String -> {String, [String String]} -> Int
+(defn obtener-numero-ciclos-tarea-2 [cadena nodos]
+  (let [nodo (obtener-lista-inicial nodos)]
+   (map #(obtener-numero-ciclos-tarea-1 cadena nodos % termina-en-z?) nodo)))
+
+; procesar-tarea-2 :: [String] -> Integer
+; Para ejecutar la tarea 2 en un tiempo optimo, se calcula el numero de ciclos de cada
+; elemento de la lista y se calcula el mcm de dichos ciclos para calcular cuando todos
+; los ciclos son coincidentes.
+(defn procesar-tarea-2 [lista-filas]
+  (let [cadena-programa (first lista-filas)
+        dict-nodos (obtener-datos (drop 2 lista-filas))
+        lista-numero-ciclos (->> dict-nodos
+                                 (obtener-numero-ciclos-tarea-2 cadena-programa)
+                                 (map #(bigint %)))
+        gcd (reduce #(gcd %1 %2) lista-numero-ciclos) ; mcd de los numeros de la lista
+        producto (reduce * lista-numero-ciclos)] 
+    (/ producto gcd)))   ; Para calcular el mcm, se divide el producto entre el mcd.
+
+;;;;;;;;;;;;;;;;;;;;; programa principal ;;;;;;;;;;;;;;;;
+
+(defn -main [& _args]
   (let [entrada-tareas (->> "./resources/input.lst"
                             (slurp)
                             (st/split-lines))
-        salida-tarea-1 (procesar-tarea-1 entrada-tareas)]
-    (println salida-tarea-1)))
+        salida-tarea-1 (procesar-tarea-1 entrada-tareas)
+        salida-tarea-2 (procesar-tarea-2 entrada-tareas)]
+    (println salida-tarea-1)
+    (println salida-tarea-2)))
--- day-8/test/day_8/core_test.clj
+++ day-8/test/day_8/core_test.clj
@@ -18,11 +18,11 @@
                                      "DDD = (DDD, DDD)"
                                      "EEE = (EEE, EEE)"
                                      "GGG = (GGG, GGG)"
-                                     "ZZZ = (ZZZ, ZZZ)"]) 
-               nodos1)))
+                                     "ZZZ = (ZZZ, ZZZ)"])
+                nodos1)))
   (test/testing "probando obtener-nuevo-nodo"
-    (test/is (= (core/obtener-nuevo-nodo nodos1 ["AAA"] "L") ["BBB"]))
-    (test/is (= (core/obtener-nuevo-nodo nodos1 ["CCC"] "R") ["GGG"]))) 
+    (test/is (= (core/obtener-nuevo-nodo nodos1 "AAA" "L") "BBB"))
+    (test/is (= (core/obtener-nuevo-nodo nodos1 "CCC" "R") "GGG")))
   (test/testing "probando procesar-tarea-1"
     (test/is (= (core/procesar-tarea-1 ["RL"
                                         ""
@@ -37,4 +37,15 @@
                                         ""
                                         "AAA = (BBB, BBB)"
                                         "BBB = (AAA, ZZZ)"
-                                        "ZZZ = (ZZZ, ZZZ)"]) 6))))
+                                        "ZZZ = (ZZZ, ZZZ)"]) 6)))
+  (test/testing "probando procesar-tarea-2"
+    (test/is (= (core/procesar-tarea-2 ["LR"
+                            ""
+                            "11A = (11B, XXX)"
+                            "11B = (XXX, 11Z)"
+                            "11Z = (11B, XXX)"
+                            "22A = (22B, XXX)"
+                            "22B = (22C, 22C)"
+                            "22C = (22Z, 22Z)"
+                            "22Z = (22B, 22B)"
+                            "XXX = (XXX, XXX)"]) 6))))
